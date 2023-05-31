a065eee8f364691c809dbbbe21c3fdf1fac8557e
suzana <suzana@adxspace.com.br>
2023-05-31 17:05:19 -0300

    criando uma lista de objst para ordernar

---
+++ listaDeCompras.js
@@ -0,0 +1,14 @@
+const listaDeComprasComPreco = [
+    {produto: "manteiga",     preco: 19.50},
+    {produto: "alcatra",      preco: 40   },
+    {produto: "leite em pó",  preco: 23   },
+    {produto: "picanha",      preco: 252  },
+    {produto: "costelinha",   preco: 235  },
+    {produto: "cuscuz",       preco: 75   },
+    {produto: "banana prata", preco: 25   },
+    {produto: "manjericão",   preco: 34   },
+    {produto: "chá verde",    preco: 65.3 },
+    {produto: "chá mate",     preco: 15.99}, 
+] 
+
+module.exports = listaDeComprasComPreco
