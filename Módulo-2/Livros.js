13c3ccca092ec5a1d459ef84c3dfc711f7e02acc
suzana-space <suzana@adxspace.com.br>
2023-06-15 15:22:12 -0300

    modulo 2

---
+++ Módulo-2/Livros.js
@@ -0,0 +1,12 @@
+ const LPs = [
+    {nome: 'PHP', preco: 30},
+    {nome: 'Javascript', preco: 50},
+    {nome: 'Java', preco: 60},
+    {nome: 'Rubi', preco: 32},
+    {nome: 'Pascal', preco: 56},
+    {nome: 'Lua', preco: 45},
+    {nome: 'Basic', preco: 12},
+    {nome: 'Python', preco: 89},
+]
+
+export default LPs
