fb32357dc65a67afe6e4f7dc30ce7d4b67c46c4a
suzana-space <suzana@adxspace.com.br>
2023-06-19 18:34:41 -0300

    quick funcionando

--- Módulo-2/QuickSort.js
+++ Módulo-2/QuickSort.js
@@ -2,15 +2,15 @@ import LivrosProg from './LivrosProg.js'
 import CursosTi from './CursosTi.js'
 
 function partition(array, esquerda, direita){
-    let pivo = array[Math.floor(array.length/2)]
     let esquerdaAtual = esquerda
     let direitaAtual = direita
+    let pivo = array[Math.floor((esquerdaAtual+direitaAtual)/2)]
 
     while(esquerdaAtual <= direitaAtual){
-        while(array[esquerdaAtual].mensalidade < array[pivo].mensalidade){
+        while(array[esquerdaAtual].mensalidade < pivo.mensalidade){
             esquerdaAtual++
         }
-        while(array[direitaAtual].mensalidade < array[pivo].mensalidade){
+        while(array[direitaAtual].mensalidade > pivo.mensalidade){
             direitaAtual--
         }
 
@@ -20,20 +20,7 @@ function partition(array, esquerda, direita){
             direitaAtual--
         }
     }
-    return array
-}
-
-
-function menoresqPivo(array, pivo){
-    let menores = 0
-
-    for(let atual = 0; atual < array.length; atual++){
-        let produtoAtual = array[atual]
-            if(produtoAtual.mensalidade < pivo.mensalidade){
-                menores++
-            }
-    }
-    return menores
+    return esquerdaAtual
 }
 
 function swap(array, de, para){
@@ -44,7 +31,7 @@ function QuickSort(array, esquerda, direita){
     if(array.length > 1){
         let indexAtual = partition(array, esquerda, direita)
             if(esquerda < indexAtual - 1){
-                QuickSort(array, esquerda, indexAtual)
+                QuickSort(array, esquerda, indexAtual - 1)
             }
             if(indexAtual < direita){
                 QuickSort(array, indexAtual, direita)
@@ -54,4 +41,5 @@ function QuickSort(array, esquerda, direita){
 }
 
 console.log(QuickSort(CursosTi, 0, CursosTi.length - 1))
+
 
