ed7e5cb715b7b185ecafaee0cf10490098595b10
suzana-space <suzana@adxspace.com.br>
2023-06-23 15:20:40 -0300

    busca binaria imcompleta esto cansada

--- Módulo-2/BuscaBinaria.js
+++ Módulo-2/BuscaBinaria.js
@@ -1,3 +1,12 @@
 import result from './QuickSort.js'
 
+const buscaBinaria = (array, esquerda, direita, valor) => {
+    if(direita >= esquerda){
+        const meio = parseFloat( essquerda + (direita+esquerda)/2)
+        if(array[meio] == valor) {
+             return valor
+        }
+    }
+}
 
+console.log(buscaBinaria(result, ))
